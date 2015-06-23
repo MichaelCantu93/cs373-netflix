8d647a9434bc873727f001fe854bdfc3f7f83f30
Michael Cantu <mc44696@cs.utexas.edu>
2015-06-22 19:15:32 -0500

    netflix code runs, cache's import works, fixed issue #8 and #15

--- Netflix.py
+++ Netflix.py
@@ -12,62 +12,52 @@
 #import netflix-tests
 #r = netflix-tests.get('https://raw.githubusercontent.com/cs373-summer-2015/netflix-tests/master/pam2599-probe_solutions.json')
 
-"""
-import urllib, json, urllib.request
-url1 = "http://www.cs.utexas.edu/~ebanner/netflix-tests/BRG564-Average_Movie_Rating_Cache.json"
-url2 = "http://www.cs.utexas.edu/~ebanner/netflix-tests/pam2599-probe_solutions.json"
-response1 = urllib.urlopen(url1)
-response2 = urllib.urlopen(url2)
-average_cache = json.loads(response1.read())
-predict_cache = json.loads(response2.read())
-"""
 
+import urllib, json
 import requests
 from math import sqrt
+from operator import add
 from functools import reduce
-r1 = requests.get('http://www.cs.utexas.edu/~ebanner/netflix-tests/BRG564-Average_Movie_Rating_Cache.json')
-r2 = requests.get('http://www.cs.utexas.edu/~ebanner/netflix-tests/pam2599-probe_solutions.json')
-average_cache = r1.json()
-predict_cache = r2.json()
+from urllib.request import urlopen
 
 
-global current_movie
-current_movie = ""
+url1 = urlopen("http://www.cs.utexas.edu/~ebanner/netflix-tests/ezo55-Average_Viewer_Rating_Cache.json")
+url2 = urlopen("http://www.cs.utexas.edu/~ebanner/netflix-tests/pam2599-probe_solutions.json")
+average_cache = json.loads(url1.read().decode(url1.info().get_param('charset') or 'utf-8'))
+predict_cache = json.loads(url2.read().decode(url2.info().get_param('charset') or 'utf-8'))
 
 global rmse_val
 rmse_val = []
 
-global count
 count = 0
 
 
 # ------------
 # netflix_eval
 # ------------
-def netflix_eval (viewer) :
+def netflix_eval (viewer, current_movie) :
+    global count
+    if "\n" in viewer:
+        viewer = viewer[:-1]
     rating = average_cache[viewer]      #simplest will use the users average ratings
     temp = predict_cache[current_movie]
     netflix_rating = temp[viewer]
-    rmse_val += (rating - netflix_rating) ** 2
-    count += 1
-    return rating[0]
+    rmse_val.extend([(rating - netflix_rating) ** 2])
+    count = count + 1
+    return '%.2f' % round(rating, 2)
 
 
 # ------------
 # netflix_solve 
 # ------------
 def netflix_solve (r, w) :
+    current_movie = ""
     for s in r :
         if ":" not in s:
-            v    = netflix_eval(s)
-            w.write(v + "\n");
+            v    = netflix_eval(s, current_movie)
+            w.write(str(v) + "\n")
         else:
             current_movie = s
-            w.write(current_movie + "\n");
-    w.write("RMSE = " + sqrt(reduce(add, rmse_val, 0)/count) + "\n");
-        
-        
-        
-        
-        
-        
+            current_movie = current_movie[:-2]
+            w.write(current_movie + ":\n")
+    w.write("RMSE = " + str('%.3f' % round(sqrt(reduce(add, rmse_val, 0)/count), 3)) + "\n")
